b79e8f8fcab1b3758176148bad9d1a86f5020844
Jocelyn <72670056+tornozelo@users.noreply.github.com>
2020-10-18 19:47:14 +0100

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -14,12 +14,13 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-let temperatureElement = document.querySelector("#temperature");
+    let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
 let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
@@ -27,9 +28,12 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "7f0e26ebcc0056eeecaacb8107edefb1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${apiKey}&units=metric`;
+let city = "Liverpool";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
